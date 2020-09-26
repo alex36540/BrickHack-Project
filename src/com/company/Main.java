97c2ddcba154d334355f8f16096d1eb9e8794c44
Alexander Lee <al3774@g.rit.edu>
2020-09-26 15:18:34 -0400

    Main class updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Main_Menu start_menu = new Main_Menu();
+        init mainframe = new init();
+        mainframe.nextMonth(jqfwjioppjiowqfpojiweqfjio);
+
+
+    }
+}
+class init {
+    public init()
+    {
+
+    }
+    public void nextMonth(ArrayList<Company> companies)
+    {
+        for(Company company : companies)
+        {
+            company.bill();
+            company.update();
+            // wait for customers to pay here
+            company.lateness();
+        }
     }
 }

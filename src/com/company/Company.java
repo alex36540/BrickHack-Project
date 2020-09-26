3935bb9bd3a56fb4375be528bfb0dbc7bcae4509
Alexander Lee <al3774@g.rit.edu>
2020-09-26 12:37:59 -0400

    Started class

--- src/com/company/Company.java
+++ src/com/company/Company.java
@@ -1,4 +1,14 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Company {
+    private String name;
+    private double totalCost;
+    private ArrayList<Customer> customers;
+    private ArrayList<PowerPlant> plants;
+
+    public Company()
+    {
+    }
 }
